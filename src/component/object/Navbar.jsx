7003f12bf93ed96274e5833dd4bbf3b6d2c42831
rohan <rohankumarpandey234@gmail.com>
2025-03-21 22:44:28 +0530

    Added scroll to top to admin button

--- src/component/object/Navbar.jsx
+++ src/component/object/Navbar.jsx
@@ -61,6 +61,7 @@ function Navbar() {
           <Button
             borderRadius="1.75rem"
             className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            onClick={handleClick}
           >
             Admin
           </Button>
